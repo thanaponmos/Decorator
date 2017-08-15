c93f4879c0fe3a773a5758a9d04383a2f1ca47a9
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-08-15 15:58:57 +0700

    Add Beverage Class
    Add attribute and abstact method

---
+++ src/Decorator/Beverage.java
@@ -0,0 +1,14 @@
+package Decorator;
+
+public abstract class Beverage {
+   
+	String description = "Unknow Beverage";
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public abstract double cost();
+	
+	
+}
