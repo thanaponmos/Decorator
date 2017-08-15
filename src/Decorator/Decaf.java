1a1bbf87f7acbfce66d8d13d86477c4b975b0f76
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-08-15 19:07:00 +0700

    Add DarkRoast Class and Decaf Class

---
+++ src/Decorator/Decaf.java
@@ -0,0 +1,14 @@
+package Decorator;
+
+public class Decaf extends Beverage {
+	public Decaf(){
+		description = "Decaf";
+	}
+	
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 1.05;
+	}
+
+}
