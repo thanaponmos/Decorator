e222798b198c9d2f8b324737ca3021c37b8712b5
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-08-15 16:34:33 +0700

    Add Mocha Class  and Whip Class

---
+++ src/Decorator/Whip.java
@@ -0,0 +1,21 @@
+package Decorator;
+
+public class Whip extends CondimentDecorator {
+
+	
+     Beverage beverage;
+	
+	public Whip(Beverage beverae){
+		this.beverage = beverae;
+	}
+
+     @Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+	return beverage.getDescription()+", Whip";
+    }
+ 
+    public double cost(){
+	   return 0.10 + beverage.cost();
+      }
+        }
