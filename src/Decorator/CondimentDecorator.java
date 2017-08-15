42dfe641a10f25d8fab0a52b050b33f1ef4f6e2b
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-08-15 16:07:53 +0700

    Add CondimentDecorator Class

---
+++ src/Decorator/CondimentDecorator.java
@@ -0,0 +1,14 @@
+package Decorator;
+
+public abstract class CondimentDecorator extends Beverage {
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public abstract String getDescription();
+	
+
+}
