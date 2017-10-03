3e609bc1548188f34a410d0b894a19992c819811
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-10-03 15:21:24 +0700

    Add

--- src/Decorator/StarBuzzCoffee.java
+++ src/Decorator/StarBuzzCoffee.java
@@ -1,28 +1,39 @@
 package Decorator;
-
+import java.text.NumberFormat;
 public class StarBuzzCoffee {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        NumberFormat numFormat = NumberFormat.getInstance();
+        
 		Beverage b1 = new Espresso();
-		System.out.println(b1.getDescription()+ " $ = "+ b1.cost() );
+		String str_b1 = numFormat.format(b1.cost());
+		System.out.println(b1.getDescription()+ " $ = "+ str_b1 );
 		
 		Beverage b2 = new HouseBlend();
 		b2 = new Mocha(b2);
 		b2 = new Whip(b2);
-		System.out.println(b2.getDescription()+ " $ = "+ b2.cost() );
+		String str_b2 = numFormat.format(b2.cost());
+		System.out.println(b2.getDescription()+ " $ = "+ str_b2 );
 		
 		Beverage b3 = new DarkRoast();
 		b3 = new Milk(b3);
 		b3 = new Soy(b3);
 		b3 = new Whip(b3);
-		System.out.println(b3.getDescription()+ " $ = "+ b3.cost() );
+		String str_b3 = numFormat.format( b3.cost());
+		System.out.println(b3.getDescription()+ " $ = "+ str_b3);
 		
 		Beverage b4 = new DarkRoast();
 		b4 = new Mocha(b4);
 		b4 = new Soy(b4);
-		System.out.println(b4.getDescription()+ " $ = "+ b4.cost() );
+		String str_b4 = numFormat.format( b4.cost());
+		System.out.println(b4.getDescription()+ " $ = "+ str_b4);
+		
+		
+    
+		
+		
+		
 	}
 
 }
